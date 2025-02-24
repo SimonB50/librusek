af9fb9fe938843e4fb8a170bc1195def58a13654
SimonB50 <szymon.dawid@gmail.com>
2025-02-24 13:25:31 +0100

    Improved developer page

--- pages/developer.js
+++ pages/developer.js
@@ -14,11 +14,48 @@ const Developer = () => {
           Tools for developers to debug and test Librus API.
         </span>
       </div>
-      <div className="flex flex-col md:flex-row gap-2 mt-4">
+      <form
+        className="flex flex-col md:flex-row gap-2 mt-4"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          setResponse(null);
+          if (!msgAuth) {
+            const response = await fetch(
+              "https://synergia.librus.pl/wiadomosci3"
+            );
+            if (!response.ok)
+              return setResponse(
+                "Error: " + response.status + " " + response.statusText
+              );
+            setMsgAuth(true);
+          }
+          const response = await fetch(
+            `${document.querySelector("#baseUrl").value}${
+              document.querySelector("#path").value
+            }`,
+            { method: "GET", credentials: "include" }
+          );
+          let responseText = "";
+          if (!response.ok) {
+            responseText +=
+              "Error: " + response.status + " " + response.statusText;
+            try {
+              responseText +=
+                "\n" + JSON.stringify(await response.json(), null, 2);
+            } catch (e) {
+            } finally {
+              return setResponse(responseText);
+            }
+          }
+          responseText +=
+            "Success: " + response.status + " " + response.statusText;
+          const responseData = await response.json();
+          responseText += "\n" + JSON.stringify(responseData, null, 2);
+          setResponse(responseText);
+        }}
+      >
         <select id="baseUrl" className="select select-bordered w-full md:w-fit">
-          <option
-            value={"https://synergia.librus.pl/gateway/api/2.0/"}
-          >
+          <option value={"https://synergia.librus.pl/gateway/api/2.0/"}>
             https://synergia.librus.pl/gateway/api/2.0/
           </option>
           <option value={"https://wiadomosci.librus.pl/api/"}>
@@ -32,35 +69,29 @@ const Developer = () => {
             placeholder="Request path"
             className="input input-bordered w-full"
           />
-          <button
-            className="btn btn-primary"
-            onClick={async () => {
-              setResponse(null);
-              if (!msgAuth) {
-                const response = await fetch("https://synergia.librus.pl/wiadomosci3");
-                if (!response.ok) return setResponse("Error: " + response.status + " " + response.statusText);
-                setMsgAuth(true);
-              }
-              const response = await fetch(
-                `${document.querySelector("#baseUrl").value}${
-                  document.querySelector("#path").value
-                }`,
-                { method: "GET", credentials: "include" }
-              );
-              if (!response.ok)
-                return setResponse(
-                  "Error: " + response.status + " " + response.statusText
-                );
-              const responseData = await response.json();
-              setResponse(JSON.stringify(responseData, null, 2));
-            }}
-          >
+          <button type="submit" className="btn btn-primary">
             GET
           </button>
         </div>
-      </div>
+      </form>
       <div className="mockup-code w-full rounded-md mt-4">
-        {response ? <pre>{response}</pre> : <pre>Awaiting response</pre>}
+        {response ? (
+          response.split("\n").map((line, i) => (
+            <pre
+              key={i}
+              data-prefix={i > 0 ? i : "~"}
+              className={`${line.startsWith("Success") ? "text-success" : ""} ${
+                line.startsWith("Error") ? "text-error" : ""
+              }`}
+            >
+              <code>{line}</code>
+            </pre>
+          ))
+        ) : (
+          <pre data-prefix="1">
+            <code>Awaiting response</code>
+          </pre>
+        )}
       </div>
     </Layout>
   );
